3577e7e79e775e7455204cc3641895055c2efa30
hen6003 <henrysmith6003@gmail.com>
2020-09-08 23:36:39 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flago"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	flags := []string{"r", "x", "u", "help"}
+
+	flagsBool, _ := flago.CheckFlags(flags)
+
+	if flagsBool[3] {
+		fmt.Println("Convert decimal to hex.\n\nOptions:\n  -r:      reverse, hex to decimal\n  -x:      include '0x' before\n  -u:      uppercase the output\n  --help:  show this help")
+	}
+
+	var str string
+	if flagsBool[1] {
+		str = "%#"
+	} else {
+		str = "%"
+	}
+
+	if flagsBool[2] {
+		str += "X"
+	} else {
+		str += "x"
+	}
+
+	if flagsBool[0] {
+		for _, v := range flago.NonFlags() {
+			vint, err := strconv.ParseInt(v, 16, 0)
+			if err == nil {
+				fmt.Printf("%d\n", vint)
+			}
+		}
+	} else {
+		for _, v := range flago.NonFlags() {
+			vint, err := strconv.Atoi(v)
+			if err == nil {
+				fmt.Printf(str+"\n", vint)
+			}
+		}
+	}
+}
